bac9492b5c2fdf617e60e90667ce616b611da7c0
pedroargento <peter.argent@gmail.com>
2013-11-25 14:27:54 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dogsoutandabout::Application.config.secret_key_base = '19a9d6b1243ebd02b69104e6fbbe02d2b6e236d35180d1d29f4ceeb8b291be7b0028034a072b50ae08eb34a462db0bc8f6398df96fb65764a46b72fb762a696f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dogsoutandabout::Application.config.session_store :cookie_store, key: '_dogsoutandabout_session'
